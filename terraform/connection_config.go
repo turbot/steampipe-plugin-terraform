41edcc64f5853512dd1297a572054bf51b9d0deb
misraved <ved@turbot.com>
2023-12-12 21:29:33 +0530

    Fix the plugin's connection config variable reference to use hcl syntax

--- terraform/connection_config.go
+++ terraform/connection_config.go
@@ -5,10 +5,10 @@ import (
 )
 
 type terraformConfig struct {
-	ConfigurationFilePaths []string `cty:"configuration_file_paths" steampipe:"watch"`
-	Paths                  []string `cty:"paths" steampipe:"watch"`
-	PlanFilePaths          []string `cty:"plan_file_paths" steampipe:"watch"`
-	StateFilePaths         []string `cty:"state_file_paths" steampipe:"watch"`
+	ConfigurationFilePaths []string `hcl:"configuration_file_paths" steampipe:"watch"`
+	Paths                  []string `hcl:"paths" steampipe:"watch"`
+	PlanFilePaths          []string `hcl:"plan_file_paths" steampipe:"watch"`
+	StateFilePaths         []string `hcl:"state_file_paths" steampipe:"watch"`
 }
 
 func ConfigInstance() interface{} {
